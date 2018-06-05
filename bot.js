1b7b90e12dad8939abdec7b84b1622d7b417c2f0
victor hugo <victorhugorch@gmail.com>
2018-06-05 11:57:00 -0300

    add exports messages with pt-br example

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 let twit = require('twit');
 let config = require('./config.js');
+let messages = require('./messages.js');
 
 const Bot = new twit(config);
 const stream = Bot.stream('statuses/filter', { track: ['quero morrer', 'vou me matar'] });
