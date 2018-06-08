c30f89b2949104620d44b5b803a72f00729dc431
victor hugo <victorhugorch@gmail.com>
2018-06-08 12:04:06 -0300

    add natural to trying using nlp in bot

--- bot.js
+++ bot.js
@@ -1,8 +1,11 @@
-const twit = require('twit');
-require('dotenv').load();
+const twit     = require('twit');
+const natural  = require('natural');
 
 const messages = require('./messages.js');
 const keywords = require('./keywords.js');
+
+const tokenizer = new natural.TreebankWordTokenizer();
+require('dotenv').load();
 
 const Bot = new twit({
   consumer_key: process.env.BOT_CONSUMER_KEY,
@@ -15,12 +18,18 @@ let keywords_arr = Object.values(keywords);
 let stream = Bot.stream('statuses/filter', { track: keywords_arr });
 
 stream.on('tweet', (tweet) => {
+  console.log(tweet.text);
+  let a = tokenizer.tokenize(tweet.text);
+  console.log(a);
+
+  /*
   let params = {
     status: '@' + tweet.user.screen_name + messageInUserLanguage(tweet.user.lang),
     in_reply_to_status_id: tweet.id_str
   };
 
   messageInUserLanguage(tweet.user.lang) != undefined ? sendTweetForUser(params) : '';
+  */
 });
 
 function messageInUserLanguage(lang) {
